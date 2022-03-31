f86a92b92149186f23b2f966b3ed59e59ed2a21e
BorjaFdzAn <BorjaFdzAn@gmail.com>
2022-03-31 18:18:23 +0200

    Asignacion posicion segun equipos

--- New Unity Project/Assets/Scripts/ControladorGUI.cs
+++ New Unity Project/Assets/Scripts/ControladorGUI.cs
@@ -6,18 +6,38 @@ using UnityEngine;
 public class ControladorGUI : MonoBehaviour
 {
     public GameObject MenuPrincipal;
-    public void OnClickBtnCliente(){
+    public void OnClickBtnCliente()
+    {
         NetworkManager.Singleton.StartClient();
         MenuPrincipal.SetActive(false);
     }
 
-    public void OnClickBtnHost(){
+    public void OnClickBtnHost()
+    {
         NetworkManager.Singleton.StartHost();
         MenuPrincipal.SetActive(false);
     }
 
-    public void OnClickBtnServidor(){
+    public void OnClickBtnServidor()
+    {
         NetworkManager.Singleton.StartServer();
         MenuPrincipal.SetActive(false);
+    }
+
+    public void OnClickEquipo1()
+    {
+        NetworkObject playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+        Jugador jugador = playerObject.GetComponent<Jugador>();
+        jugador.PosicionAleatoriaJugadorEquipo1ServerRpc();
+    }
+    public void OnClickEquipo2()
+    {
+        NetworkObject playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+        Jugador jugador = playerObject.GetComponent<Jugador>();
+        jugador.PosicionAleatoriaJugadorEquipo2ServerRpc();
+    }
+    public void OnClickEquipoSinEquipo()
+    {
+
     }
 }
--- New Unity Project/Assets/Scripts/Jugador.cs
+++ New Unity Project/Assets/Scripts/Jugador.cs
@@ -20,11 +20,20 @@ public class Jugador : NetworkBehaviour
         this.transform.position = nuevaPosicion;
     }
 
-
-
     [ServerRpc]
     private void PosicionAleatoriaJugadorInicioServerRpc(ServerRpcParams parametros = default){
         this.posicionJugador.Value = DevolverPosicionAleatoriaCentroPlano();
+    }
+    
+    [ServerRpc]
+    public void PosicionAleatoriaJugadorEquipo1ServerRpc(ServerRpcParams parametros = default){
+        Vector3 posicionAleatoriaIzquierda = new Vector3(Random.Range(-5, 5), 0, Random.Range(-2, -5));
+        this.posicionJugador.Value = posicionAleatoriaIzquierda;
+    }
+    [ServerRpc]
+    public void PosicionAleatoriaJugadorEquipo2ServerRpc(ServerRpcParams parametros = default){
+        Vector3 posicionAleatoriaDerecha = new Vector3(Random.Range(-5, 5), 0, Random.Range(2, 5));
+        this.posicionJugador.Value = posicionAleatoriaDerecha;
     }
     private Vector3 DevolverPosicionAleatoriaCentroPlano(){
         return new Vector3(Random.Range(-5, 5), 0, Random.Range(-1, 1));
